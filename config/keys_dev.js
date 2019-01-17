e8a82a70ed8550e86802f79487bf182e8621ca09
kaikaine <kairijb@protonmail.com>
2019-01-17 08:29:42 -0600

    added a databse and set up the server

---
+++ config/keys_dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI: 'mongodb://kairi:kairi123@ds139685.mlab.com:39685/hughniq',
+    secret: 'secret'
+}
---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const passport = require('passport')
+const app = express()
+
+// Body parser
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json())
+
+// DB
+const db = require('./config/keys').mongoURI
+
+// connect to mongoose
+mongoose.connect(db, { useNewUrlParser: true })
+.then(() => console.log('MongoDB Connected'))
+.catch(err => console.log(err))
+
+// Passport middleware
+app.use(passport.initialize())
+
+
+// Serve static assets if in production
+if(process.env.NODE_ENV === 'production') {
+    // set static folder
+    app.use(express.static('client/build'))
+    app.get('*', (req,res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+    console.log("App is running on port " + port);
+});
